5cd444fd7fe67e230ec6591766d2eb2985dfe9e2
mohmutho <mohammad.mutho@gmail.com>
2022-01-24 09:14:10 +0700

    first commit

---
+++ custom/training/trainingbackoffice/gensrc/org/trainingbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jan 21, 2022, 4:23:44 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.trainingbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "trainingbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
